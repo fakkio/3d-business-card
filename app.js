8439357e958bb868003b88776754d6928e733ab4
Fabio <lazzaroni@jeflab.it>
2020-11-05 16:13:39 +0100

    📱 add touch support

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 const card = document.querySelector(".card");
 const container = document.querySelector(".container");
 
-container.addEventListener("mousemove", (e) => {
+let touchEndTimer;
+
+const mouseMove = (e) => {
+  let touch;
+  if (e.changedTouches) {
+    touch = e.changedTouches[0];
+  }
+
   const centerX = window.innerWidth / 2;
   const centerY = window.innerHeight / 2;
-  const deltaX = e.pageX - centerX;
-  const deltaY = e.pageY - centerY;
+  const deltaX = (e.pageX || touch.pageX) - centerX;
+  const deltaY = (e.pageY || touch.pageY) - centerY;
   const rotateY = deltaX / 15;
   const rotateX = -deltaY / 15;
 
@@ -13,15 +20,37 @@ container.addEventListener("mousemove", (e) => {
 
   card.style.transform = `rotateY(${rotateY}deg) rotateX(${rotateX}deg)`;
   card.style.boxShadow = `0 ${shadowY}px 20px rgba(0, 0, 0, 0.2), 0 0 50px rgba(0, 0, 0, 0.2)`;
-});
+};
 
-container.addEventListener("mouseenter", () => {
+const mouseEnter = () => {
   card.style.transition = "none";
-});
+};
 
-container.addEventListener("mouseleave", () => {
+const mouseLeave = () => {
   card.style.transition = "all 0.5s ease";
   card.style.transform = "rotateY(0) rotateX(0)";
   card.style.boxShadow =
     "0 20px 20px rgba(0, 0, 0, 0.2), 0 0 50px rgba(0, 0, 0, 0.2)";
-});
+};
+
+container.addEventListener("mousemove", mouseMove);
+container.addEventListener("mouseenter", mouseEnter);
+container.addEventListener("mouseleave", mouseLeave);
+
+container.addEventListener("touchmove", mouseMove, {passive: true});
+container.addEventListener(
+  "touchstart",
+  () => {
+    mouseEnter();
+    clearTimeout(touchEndTimer);
+  },
+  {passive: true}
+);
+container.addEventListener(
+  "touchend",
+  () => {
+    mouseLeave();
+    touchEndTimer = setTimeout(mouseLeave, 200);
+  },
+  {passive: true}
+);
